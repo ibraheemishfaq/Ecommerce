e3bd91dc95c33a7cdc5f54e4f7ac65a9b816395a
HP <HP@DESKTOP-H9PPBLH>
2018-08-18 17:46:51 +0500

    initialize project

---
+++ ECOMMERCE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECOMMERCE.Startup))]
+namespace ECOMMERCE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
